6ef5dceef1e82f25d4f7f7eaaf3d51cd76951636
@cegn <cegn@senado.leg.br>
2023-11-09 14:58:15 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import vituum from 'vituum'
+import pug from '@vituum/vite-plugin-pug'
+import path from 'node:path'
+
+export default {
+  plugins: [vituum(), pug({
+      root: './src'
+  })],
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
+}
